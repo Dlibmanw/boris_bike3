1e11badbb373bb335cf9a30b12397f1dbc13b01b
hansa-lallu <hansalallu@gmail.com>
2020-01-22 14:32:43 +0000

    passed step 8

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,5 +1,6 @@
 # Define filenmae to run test on
-require "docking_station"
-describe DockingStation do
+require 'docking_station'
 
+describe DockingStation do
+    it { should respond_to(:release_bike) }
 end
