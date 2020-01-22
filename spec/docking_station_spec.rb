4bef930c60b1aa7355f5798965d861f083a9b44d
DavidStewartLDN <davidstewart.work@gmail.com>
2020-01-22 14:22:33 +0000

    Step 8

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,5 +1,7 @@
 # Define filenmae to run test on
 require "docking_station"
 describe DockingStation do
-
+  context "check if release bike method exists" do
+    it { is_expected.to respond_to(:release_bike) }
+  end
 end
