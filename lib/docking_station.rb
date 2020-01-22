c2f2941cad841c467f228a66f83270e397c426b2
DavidStewartLDN <davidstewart.work@gmail.com>
2020-01-22 17:43:23 +0000

    step 10 - completed

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,5 +1,7 @@
+require '/Users/davide/Projects/boris_bikes/lib/bike.rb'
+
 class DockingStation
   def release_bike
-
+    bike = Bike.new
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,4 +4,14 @@ describe DockingStation do
   context "check if release bike method exists" do
     it { is_expected.to respond_to(:release_bike)}
   end
+  context "check that release bike method to be working" do
+    it "is true" do
+      expect(subject.release_bike).to be_working
+    end
+  end
+  # context "check bike class is working" do
+  #   it "is true" do
+  #     expect(Bike.new).to exist
+  #   end
+  # end
 end
