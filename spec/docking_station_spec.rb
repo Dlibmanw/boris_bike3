8ff0f54220b5326c42e13b5758ec3a9f63e79635
Sophie Worsnop <sophie.g.worsnop@gmail.com>
2020-01-23 15:57:45 +0000

    Half way

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,15 +1,23 @@
 require 'docking_station'
 
-
 describe DockingStation do
     it { should respond_to(:release_bike) }
-
 
     it 'releases a new bike if bike is working' do
        bike = subject.release_bike
        expect(bike).to be_working
     end
-end
 
+   # 'docks bike when possible'
+    it { should respond_to(:dock).with(1).argument }
+      # dock = subject.dock
+      # expect(bike).to be_dock
 
-sophie is cool
+    it { should respond_to(:bike) }
+
+    it 'docks something' do
+      bike = Bike.new
+      expect(subject.dock(bike)).to eq(true)
+    end
+
+    end
